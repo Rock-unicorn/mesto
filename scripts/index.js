d0d73c99d248b2cada1408788b1170e11893ec3c
Dmitry Tsygankov <rockcorn@yandex.ru>
2022-11-29 20:45:18 +0300

    2d init

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,34 @@
 const changeProfile = document.querySelector('.profile__info-change-button');
 const popup = document.querySelector('.popup');
-const formClose = document.querySelector('.form__close-button');
-const saveButton = document.querySelector('.form');
+const formClose = document.querySelector('.popup__close-button');
+const formSave = document.querySelector('.form');
 let infoName = document.querySelector('.profile__name');
 let infoProfession = document.querySelector('.profile__profession');
-let nameContainer = document.querySelector('.form__input-name');
-let professionContainer = document.querySelector('.form__input-profession');
-nameContainer.defaultValue = infoName.textContent;
-professionContainer.defaultValue = infoProfession.textContent;
+let nameContainer = document.querySelector('.form__input_data_name');
+let professionContainer = document.querySelector('.form__input_data_profession');
 
-changeProfile.addEventListener('click', function() {
+function popupOpen() {
     popup.classList.add('popup_opened');
-})
+    nameContainer.value = infoName.textContent;
+    professionContainer.value = infoProfession.textContent;
+}
 
-formClose.addEventListener('click', function() {
+function popupClose() {
     popup.classList.remove('popup_opened');
-})
+}
+
+changeProfile.addEventListener('click', popupOpen);
+
+formClose.addEventListener('click', popupClose);
 
 function changeInfo(evt) {
     evt.preventDefault()
     infoName.textContent = nameContainer.value;
     infoProfession.textContent = professionContainer.value;
-    popup.classList.remove('popup_opened');
+    popupClose();
 
 }
 
-saveButton.addEventListener('submit', changeInfo);
+formSave.addEventListener('submit', changeInfo);
 
 
